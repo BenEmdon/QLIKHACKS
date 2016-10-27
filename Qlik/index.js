59b2e9d369fd7cdc3102d09b06e2358b4eb9d943
BenEmdon <benjaminemdon@gmail.com>
2016-10-27 11:17:04 -0400

    Added game skelton

--- Qlik/index.js
+++ Qlik/index.js
@@ -1,7 +1,13 @@
 const io = require("socket.io-client")
+const request = require("request")
 const config = require("./config")
 
 console.log("Welcome to the Qritter Wars Client")
+
+// This is the api key passed to the Qritter Wars REST API in the Authorization header
+// for authentication
+// format: base64 encoded value of <apiId>:<apiSecret>
+const apiKey = new Buffer(`${config.apiId}:${config.apiSecret}`).toString('base64')
 
 const socketArguments = `apiId=${config.apiId}&apiSecret=${config.apiSecret}`
 
@@ -16,7 +22,56 @@ socket.on('invalid', (error) => {
   console.log('error', error)
 })
 
-socket.on('success', (id) => {
+socket.on('success', (data) => {
   playerId = data.id
-  console.log(id);
 })
+
+socket.on('start game', (game) => {
+
+  getGame(game.id)
+  .then((gameData) => {
+    if(gameData.current === playerId) {
+      console.log("our turn")
+    }
+  })
+})
+
+let getGame = (gameId) => {
+  return new Promise((resolve, reject) => {
+
+    // we want to perform a GET request to the games/:id API
+    // to retrieve information about the given game
+    let options = createOptions(`games/${gameId}`, "GET")
+
+    request.get(options, (error, res, body) => {
+      if (error || res.statusCode !== 200) {
+        console.error("Error Getting Game", error || res.body)
+        reject(error)
+      } else {
+        resolve(JSON.parse(body))
+      }
+    })
+  })
+}
+
+let createOptions = (endpoint, method, body) => {
+  // we need to return all options that the request module expects
+  // for an http request. 'uri' is the location of the request, 'method'
+  // is what http method we want to use (most likely GET or POST). headers
+  // are the http headers we want attached to our request
+  let options = {
+    uri: `http://${config.host}:${config.apiPort}/${endpoint}`,
+    method: method.toUpperCase(),
+    headers: {
+      "Authorization": `Basic ${apiKey}`,
+      "Content-Type": "application/json"
+    }
+  }
+
+  if (body != null) {
+    // if a body has been specified we want to add it to the http request
+    options.body = JSON.stringify(body)
+  }
+
+  return options
+}
